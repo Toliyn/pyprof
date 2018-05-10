7a57d569e020d3e5bc98cf69b1040230ef41be64
Anatoliy Petrov <toliyn@gmail.com>
2018-05-10 17:18:43 +0300

    bot initial

---
+++ tbot/bot.py
@@ -0,0 +1,18 @@
+from telegram.ext import Updater, CommandHandler
+
+
+def greet_user(bot, updater):
+    #mytext = 'Привет пользователь'
+    print('Вызван /start')
+
+
+def main():
+    updtr = Updater('596855786:AAExs1OHdwVyH1HjHDLii00qa7DPbwt8WrU')
+    dp = updtr.dispatcher
+    dp.add_handler(CommandHandler('start', greet_user))
+    updtr.start_polling()
+    updtr.idle()
+
+
+if __name__ == '__main__':
+    main()
