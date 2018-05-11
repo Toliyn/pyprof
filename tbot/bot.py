58e53dadaace830fee6037c2372529f3d6334b52
Anatoliy Petrov <toliyn@gmail.com>
2018-05-11 11:41:21 +0300

    reply added

--- tbot/bot.py
+++ tbot/bot.py
@@ -2,12 +2,14 @@ from telegram.ext import Updater, CommandHandler
 
 
 def greet_user(bot, updater):
-    #mytext = 'Привет пользователь'
-    print('Вызван /start')
-
+    mytext = """Привет пользователь!
+    я простой бот и понимаю только команду /start
+    """
+    #print('Вызван /start')
+    updater.message.reply_text(mytext)
 
 def main():
-    updtr = Updater('')
+    updtr = Updater('596855786:AAExs1OHdwVyH1HjHDLii00qa7DPbwt8Wr')
     dp = updtr.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
     updtr.start_polling()
