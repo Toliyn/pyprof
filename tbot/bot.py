3bfc8bddee4ff98840c476fd67887892085117f8
Anatoliy Petrov <toliyn@gmail.com>
2018-05-11 17:14:24 +0300

    settings created

--- tbot/bot.py
+++ tbot/bot.py
@@ -1,4 +1,5 @@
 import logging
+import settings
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level = logging.INFO, filename='bot.log')
@@ -8,12 +9,12 @@ def greet_user(bot, updater):
     updater.message.reply_text(mytext)
 
 def chat(bot, updater):
-	text = update.message.text
+	text = updater.message.text
 	logging.info(text)
 	updater.message.reply_text(text)
 
 def main():
-    updtr = Updater('596855786:AAExs1OHdwVyH1HjHDLii00qa7DPbwt8W')
+    updtr = Updater(settings.TLG_API_KEY)
     dp = updtr.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
     dp.add_handler(MessageHandler(Filters.text, chat))
